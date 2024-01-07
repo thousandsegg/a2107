fd2e64d771c29549bd2862d95022c39c184cb3e5
15081061263 <12365730+pang-junshuai@user.noreply.gitee.com>
2024-01-07 20:17:39 +0800

    activityViewBinding

--- app/src/main/java/com/example/a2107/MainActivity.kt
+++ app/src/main/java/com/example/a2107/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.a2107
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.a2107.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+
+        
 
 
     }
