c4c5ff14be607becde9dbfaac4bb7c1cf3a8da8a
15081061263 <12365730+pang-junshuai@user.noreply.gitee.com>
2024-01-07 19:32:48 +0800

    创建项目

---
+++ app/src/main/java/com/example/a2107/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a2107
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
